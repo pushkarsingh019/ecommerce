eb76f516fc2bfda05553cc67e53ccb0020634f55
Pushkar Singh <stoazest@gmail.com>
2023-05-22 17:41:50 +0530

    product card image, css working

--- src/screens/product/Product.jsx
+++ src/screens/product/Product.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import Navbar from "../../components/navigation/Navbar";
 
 const Product = () => {
+    const { productId } = useParams();
+    const navigate = useNavigate();
+
+    if (productId === undefined) {
+        navigate("/products");
+    }
     return (
         <section>
             <Navbar />
             <h2>Product Page</h2>
+            {productId}
         </section>
     );
 };
